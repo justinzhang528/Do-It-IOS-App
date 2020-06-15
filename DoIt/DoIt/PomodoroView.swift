51617cd9037127d6ba1b2adb4bc3eb8cd0474f60
user172171 <user172171@AS004.local>
2020-06-15 23:20:16 +0800

    Pomodoro Update 1

---
+++ DoIt/DoIt/PomodoroView.swift
@@ -0,0 +1,21 @@
+//
+//  PomodoroView.swift
+//  DoIt
+//
+//  Created by user172171 on 6/14/20.
+//  Copyright © 2020 NTUT. All rights reserved.
+//
+
+import SwiftUI
+
+struct PomodoroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PomodoroView_Previews: PreviewProvider {
+    static var previews: some View {
+        PomodoroView()
+    }
+}
